c45274e57deda3dd38d45144b27a42ce8593ff54
Antoniojrsales <Antoniogomes.junio@gmail.com>
2025-06-01 18:27:47 -0300

    Refatorando

--- Bi_Gastos_Resid.py
+++ Bi_Gastos_Resid.py
@@ -17,7 +17,7 @@ st.set_page_config(
 
 @st.cache_data(ttl=600)
 #Criando a conexao com a planilha do google sheets
-def carregar_dados():
+def load_data():
     try:
         load_dotenv()
         sheet_id = os.getenv('SHEET_ID')
@@ -31,7 +31,9 @@ def carregar_dados():
         st.error(f"Erro ao carregar dados: {e}")
         return pd.DataFrame()  # Retorna DataFrame vazio como fallback
     
-df_dados = carregar_dados()
+df_dados = load_data()
+
+st.session_state['df_Bi_Gastos_Resid'] = df_dados
 
 # Lista dos tipos de despesa
 tipos_despesa = ['Despesa Moto', 'Despesa Casa', 'Despesa Combustivel', 'Despesa Remedio', 
@@ -41,6 +43,7 @@ tipos_despesa = ['Despesa Moto', 'Despesa Casa', 'Despesa Combustivel', 'Despesa
 # Lista dos tipos de despesa diarias
 tipos_despesa_diarias = ['Despesa Moto', 'Despesa Casa', 'Despesa Combustivel', 
                          'Despesa Remedio', 'Outros Laser/Festa/Reforma']
+
 
 def clean_currency(value):
     if pd.isna(value):
@@ -53,57 +56,51 @@ def clean_currency(value):
         return float(value) if value else 0.0
     return float(value)
 
-# Aplica a função e converte a coluna para float
-df_dados['Valor'] = df_dados['Valor'].apply(clean_currency).astype(float)
+def calculate_sum(df, categoria):
+    return df[df['Categorias'] == categoria]['Valor'].sum()
 
-df_dados['Categorias'] = df_dados['Categorias'].str.strip().str.title()
+def calculate_mean(df, categoria):
+    return df[df['Categorias'] == categoria]['Valor'].mean()
 
-df_dados['Data'] = pd.to_datetime(df_dados['Data'], format='%d/%m/%y', errors='coerce')
-df_dados['Mes'] = df_dados['Data'].dt.strftime('%b/%Y')  # Formato: Jan/2024
+def calculate_total(df, categoria):
+    return df[df['Categorias'] == categoria].groupby('Mes')['Valor'].sum().mean()
 
-# Funcao que Calcula o total das receitas.
-def revenues():
-    # Soma todos os 'Valor's associados à categoria 'Receita' no DataFrame 'df_dados'.
-    total_receitas = df_dados[df_dados['Categorias'] == 'Receita']['Valor'].sum()
-    
-    # Retorna o valor total das receitas.
-    return total_receitas
+def total_revenue(df):
+    # Soma os valores da categoria 'Receita'
+    return calculate_sum(df, 'Receita')
 
-# Funcao que Calcula o total das despesas
-def general_expenses():
-    # Soma todos os 'Valor's associados às categorias de despesa listadas em 'tipos_despesa' no DataFrame 'df_dados'.
+def general_expense():
+    # Soma os valores total da categoria 'despesas'
     df_despesas = df_dados[df_dados['Categorias'].isin(tipos_despesa)]    
     total_despesas = df_despesas['Valor'].sum()
     
     # Retorna o valor total das despesas.
     return total_despesas
 
-# Funcao que Filtra o total das despesas diarias
-def daily_expenses():
+def daily_expense():
     # Filtra todos os 'Valor's associados às categorias de despesa listadas em 'tipos_despesa_diarias' no DataFrame 'df_dados'.
     df_despesas_diarias = df_dados[df_dados['Categorias'].isin(tipos_despesa_diarias)]
 
-    # Retorna o valor total das despesas.
+    # Retorna o valor total das despesas diarias.
     return df_despesas_diarias
 
-def despesas_moradia():
-    total_despesas_moradia = df_dados[df_dados['Categorias'] == 'Despesa Casa']['Valor'].mean()
-    return  total_despesas_moradia
+def housing_expenses(df):
+    # Retorna uma media por semana dos valores da categoria 'despesas moradia'
+    return  calculate_mean(df, 'Despesa Casa')
 
-def despesas_remedio():
-    df_despesas_remedio = df_dados[df_dados['Categorias'] == 'Despesa Remedio']
-    total_despesas_remedio = df_despesas_remedio['Valor'].mean()
-    return  total_despesas_remedio
+def remedy_expenses(df):    
+    # Retorna uma media por mes dos valores da categoria 'despesas remedios'
+    return  calculate_total(df, 'Despesa Remedio')
 
-def despesas_combustivel():
-    df_despesas_combustivel = df_dados[df_dados['Categorias'] == 'Despesa Combustivel']
-    total_despesas_combustivel = df_despesas_combustivel['Valor'].mean()
-    return total_despesas_combustivel
+# Funcao que Filtra o total das despesas combustivel
+def fuel_expenses(df):
+    # Retorna a media diaria dos valores da categoria 'despesas combustivel'
+    return calculate_mean(df, 'Despesa Combustivel')
 
-def despesas_conserto_veicular():
-    df_despesas_conserto_veicular = df_dados[df_dados['Categorias'] == 'Despesa Moto']
-    total_despesas_conserto_veicular = df_despesas_conserto_veicular['Valor'].mean()
-    return total_despesas_conserto_veicular
+# Funcao que Filtra o total das despesas conserto veiculo
+def repair_expenses(df):   
+    # Retorna a media por mes dos valores da categoria 'Despesa Moto'
+    return calculate_total(df, 'Despesa Moto')
 
 def receitas_despesas_mensais(df):
     df = df.copy()
@@ -122,9 +119,12 @@ def render_card(title, value, gradient):
         color: white;
         padding: 20px;
         border-radius: 10px;
+        font-size: 1.2em;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
     """
     saldo_style = """
-        font-size: 2em;
+        font-size: 1.5em;
         font-weight: bold;
     """
     st.markdown(f"""
@@ -134,9 +134,17 @@ def render_card(title, value, gradient):
         </div>
     """, unsafe_allow_html=True)
 
+# Aplica a função e converte a coluna para float
+df_dados['Valor'] = df_dados['Valor'].apply(clean_currency).astype(float)
 
-# Calculando o Saldo Atual (Receita  - Despesas)
-saldo_atual = revenues() - general_expenses()
+df_dados['Categorias'] = df_dados['Categorias'].str.strip().str.title()
+
+df_dados['Data'] = pd.to_datetime(df_dados['Data'], format='%d/%m/%y', errors='coerce')
+df_dados['Mes'] = df_dados['Data'].dt.strftime('%b/%Y')  # Formato: Jan/2024
+
+# Separar receitas e despesas
+receitas_por_mes = df_dados[df_dados['Categorias'] == 'Receita'].groupby('Mes')['Valor'].sum()
+despesas_por_mes = df_dados[df_dados['Categorias'] != 'Receita'].groupby('Mes')['Valor'].sum()
 
 st.sidebar.markdown("""
     <div style="
@@ -147,37 +155,47 @@ st.sidebar.markdown("""
     </div>
 """, unsafe_allow_html=True)
 
-with st.sidebar.expander("🔍 Visualizar colunas (debug)"):
-    options = st.multiselect('Escolha a Coluna:', df_dados.columns)
-    if options:
-        df_filtrado = df_dados[options]
-        st.write('Dataframe Filtrado:', df_filtrado)
-    else:
-        st.write('Por favor, selecione ao menos uma coluna.')
+# Preparar lista de meses únicos (já formatados no estilo 'Jan/2025')
+meses_disponiveis = ['Saldo Atual'] + list(receitas_por_mes.index)
+mes_escolhido = st.sidebar.selectbox('Escolha o mês: ', meses_disponiveis, index=0)
 
 col1, col2, col3 = st.columns([1, 1, 1])
-
-with col1:
-    render_card("Saldo Atual", saldo_atual, "#FF8C00, #E91E63")
-
-with col2:
-    render_card("Receita", revenues(), "#A9A9A9, #696969")
-
-with col3:
-    render_card("Despesas", general_expenses(), "#A9A9A9, #696969")
+if mes_escolhido == 'Saldo Atual': # Obter valores total
+    # Calculando o Saldo Atual (Receita  - Despesas)
+    saldo_atual = total_revenue(df_dados) - general_expense()
+    
+    with col1:
+        render_card("Saldo Atual", saldo_atual, "#FF8C00, #E91E63")
+    with col2:
+        render_card("Receita", total_revenue(df_dados), "#A9A9A9, #696969")
+    with col3:
+        render_card("Despesas", general_expense(), "#A9A9A9, #696969")
+else: # Obter valores específicos do mês escolhido
+    receita_mes = receitas_por_mes.get(mes_escolhido, 0)
+    despesa_mes = despesas_por_mes.get(mes_escolhido, 0)
+    saldo_mes = receita_mes - despesa_mes
+    
+    with col1:
+        render_card(f"Saldo de {mes_escolhido}", saldo_mes, "#FF8C00, #E91E63")
+    with col2:
+        render_card(f"Receita de {mes_escolhido}", receita_mes, "#A9A9A9, #696969")
+    with col3:
+        render_card(f"Despesas de {mes_escolhido}", despesa_mes, "#A9A9A9, #696969")
 
 col4, col5 = st.columns([1, 1])
-
 with col4:
     st.markdown("""
         <div style="
             padding: 20px;
             text-align: center;">
-            <h3>Distribuição de Despesas Diárias</h3>
+            <h3 style=" font-size: 2em; 
+                        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
+                Distribuição de Despesas Diárias
+            </h3>
             <div id="chart-container" style="margin-top: 10px;"></div>
         </div>
     """, unsafe_allow_html=True)
-    fig = px.pie(daily_expenses(), 
+    fig = px.pie(daily_expense(), 
                     values='Valor', 
                     names='Categorias',
                     hole=0.4,
@@ -197,7 +215,10 @@ with col5:
         <div style="
             padding: 20px;
             text-align: center;">
-            <h3>Distribuição Principais Gastos</h3>
+            <h3 style=" font-size: 2em; 
+                        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
+                Distribuição Principais Gastos
+            </h3>
             <div id="chart-container"></div>
         </div>
     """, unsafe_allow_html=True)
@@ -212,7 +233,7 @@ with col5:
         </div>
         """, unsafe_allow_html=True)
         
-        render_card("Media de gastos por semana Moradia", despesas_moradia(), "#555555, #696969")
+        render_card("Media de gastos com Moradia", housing_expenses(df_dados), "#555555, #696969")
 
     with col_12:
         st.markdown("""
@@ -222,7 +243,7 @@ with col5:
         </div>
         """, unsafe_allow_html=True)
         
-        render_card("Media de gastos de Combustivel", despesas_combustivel(), "#555555, #696969")
+        render_card("Media de gastos Combustivel", fuel_expenses(df_dados), "#555555, #696969")
     
     col_21, col_22 = st.columns([1, 1])
 
@@ -234,7 +255,7 @@ with col5:
         </div>
         """, unsafe_allow_html=True)
         
-        render_card("Media de gastos com Remedios", despesas_remedio(), "#555555, #696969")
+        render_card("Media de gastos com Remedios", remedy_expenses(df_dados), "#555555, #696969")
 
     with col_22:
         st.markdown("""
@@ -244,51 +265,55 @@ with col5:
         </div>
         """, unsafe_allow_html=True)
         
-        render_card("Media de gastos com Veiculo", despesas_conserto_veicular(), "#555555, #696969")
+        render_card("Media de gastos com Veiculo", repair_expenses(df_dados), "#555555, #696969")
 
-# Separar receitas e despesas
-receitas_por_mes = df_dados[df_dados['Categorias'] == 'Receita'].groupby('Mes')['Valor'].sum()
-despesas_por_mes = df_dados[df_dados['Categorias'] != 'Receita'].groupby('Mes')['Valor'].sum()
+c = st.container()
+with c:
+    st.markdown("""
+        <div style="
+            text-align: center;">
+            <h3 style=" font-size: 2em; 
+                        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
+                Distribuição de Despesas Diárias
+            </h3>
+            
+        </div>
+    """, unsafe_allow_html=True)
+    # Unir num DataFrame para facilitar o gráfico
+    df_grafico = pd.DataFrame({
+        'Receitas': receitas_por_mes,
+        'Despesas': despesas_por_mes
+    }).fillna(0)  # Preencher valores ausentes com zero
 
-# Unir num DataFrame para facilitar o gráfico
-df_grafico = pd.DataFrame({
-    'Receitas': receitas_por_mes,
-    'Despesas': despesas_por_mes
-}).fillna(0)  # Preencher valores ausentes com zero
+    # Resetar o índice para usar no Plotly
+    df_grafico = df_grafico.reset_index()
 
-# Resetar o índice para usar no Plotly
-df_grafico = df_grafico.reset_index()
+    df_grafico['Mes'] = pd.to_datetime(df_grafico['Mes'], format='%b/%Y')
+    df_grafico = df_grafico.sort_values('Mes')
+    df_grafico['Mes'] = df_grafico['Mes'].dt.strftime('%b/%Y')
 
-df_grafico['Mes'] = pd.to_datetime(df_grafico['Mes'], format='%b/%Y')
-df_grafico = df_grafico.sort_values('Mes')
-df_grafico['Mes'] = df_grafico['Mes'].dt.strftime('%b/%Y')
-
-
-fig = go.Figure(
-    data=[
-        go.Bar(x=df_grafico['Mes'], y=df_grafico['Receitas'], name='Receitas', marker_color='mediumseagreen'),
-        go.Bar(x=df_grafico['Mes'], y=df_grafico['Despesas'], name='Despesas', marker_color='indianred')
-    ]
-)
-
-fig.update_layout(
-    title="Receitas vs Despesas por Mês",
-    title_x=0.5,
-    title_font=dict(size=22),
-    xaxis_title="Mês",
-    xaxis_title_font=dict(size=18),
-    yaxis_title="Valor (R$)",
-    yaxis_title_font=dict(size=18),
-    plot_bgcolor='white',
-    legend=dict(
-        x=1.02, y=1,
-        traceorder="normal",
-        font=dict(size=12),
-        bordercolor="Black",
-        borderwidth=1
+    fig = go.Figure(
+        data=[
+            go.Bar(x=df_grafico['Mes'], y=df_grafico['Receitas'], name='Receitas', marker_color='mediumseagreen'),
+            go.Bar(x=df_grafico['Mes'], y=df_grafico['Despesas'], name='Despesas', marker_color='indianred')
+        ]
     )
-)
 
-fig.update_traces(texttemplate='%{y:.2f}', textposition='outside')
+    fig.update_layout(
+        xaxis_title="Meses",
+        xaxis_title_font=dict(size=20),
+        yaxis_title="Valor (R$)",
+        yaxis_title_font=dict(size=20),
+        plot_bgcolor='white',
+        legend=dict(
+            x=1.02, y=1,
+            traceorder="normal",
+            font=dict(size=12),
+            bordercolor="Black",
+            borderwidth=1
+        )
+    )
 
-st.plotly_chart(fig, use_container_width=True)
+    fig.update_traces(texttemplate='%{y:.2f}', textposition='outside')
+
+    st.plotly_chart(fig, use_container_width=True)
---
+++ pages/Dados.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+if 'df_Bi_Gastos_Resid' not in st.session_state:
+    st.session_state['df_Bi_Gastos_Resid'] = carregar_dados()
+
+df_original = st.session_state['df_Bi_Gastos_Resid']
+df_original
+
+with st.sidebar.expander("🔍 Visualizar colunas"):
+    options = st.multiselect('Escolha a Coluna:', df_original.columns)
+
+if options:
+    df_filtrado = df_original[options]
+    st.write('Dataframe Filtrado:', df_filtrado.tail())
+else:
+    st.write('Por favor, selecione ao menos uma coluna.')
